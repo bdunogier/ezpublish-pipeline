07accbc6bfdd687c7ae4b0b157bfc1d70139f889
Bertrand Dunogier <bd@ez.no>
2015-04-05 16:39:15 +0200

    Added more pipeline settings

---
+++ ezpublish_legacy/settings/siteaccess/pipeline_site_admin/site.ini.append.php
@@ -0,0 +1,25 @@
+<?php /* #?ini charset="utf-8"?
+
+[SiteSettings]
+SiteName=Pipeline site
+SiteURL=php55-vm.ezpublish5/index.php
+DefaultPage=content/dashboard
+LoginPage=custom
+
+[SiteAccessSettings]
+ForceVirtualHost=true
+RequireUserLogin=true
+RelatedSiteAccessList[]=pipeline_site
+ShowHiddenNodes=true
+
+[DesignSettings]
+SiteDesign=admin2
+AdditionalSiteDesignList[]=admin
+
+[ContentSettings]
+CachedViewPreferences[full]=admin_navigation_content=1;admin_children_viewmode=list;admin_list_limit=1
+
+[ContentSettings]
+ViewCaching=enabled
+
+*/ ?>
